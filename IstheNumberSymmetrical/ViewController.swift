667c7b63a1f36018870b5fd29af8c6828a1d3845
Daniel Washington <danielvertigo@hotmail.com>
2021-05-31 20:33:31 -0300

    Files

--- IstheNumberSymmetrical/ViewController.swift
+++ IstheNumberSymmetrical/ViewController.swift
@@ -5,6 +5,27 @@
 //  Created by Daniel Washington Ignacio on 31/05/21.
 //
 
+
+/*
+ Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not. A number is symmetrical when it is the same as its reverse.
+
+ Examples
+
+ isSymmetrical(7227) ➞ true
+
+ isSymmetrical(12567) ➞ false
+
+ isSymmetrical(44444444) ➞ true
+
+ isSymmetrical(9939) ➞ false
+
+ isSymmetrical(1112111) ➞ true
+ Notes
+
+ N/A
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +33,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(self.isSymmetrical(7227))
+        print(self.isSymmetrical(12567))
+        print(self.isSymmetrical(44444444))
+        print(self.isSymmetrical(9939))
+        print(self.isSymmetrical(1112111))
+    }
+    
+    func isSymmetrical(_ num: Int) -> Bool {
+        return String(num) == String(String(num).reversed())
+        
     }
 
 
